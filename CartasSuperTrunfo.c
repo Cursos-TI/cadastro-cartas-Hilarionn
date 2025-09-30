54549bd4b0962a82c789111b6cbee4152475d158
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2025-09-30 00:50:15 +0000

    Estruturação 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,75 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+  // Primeira Carta
+  char Letra_1 = 'A';
+  char Codigo_1 [20] =  "A01";
+  char Nome_da_cidade_1 [20] = "Fortaleza";
+  int populacao_1 = 500000;
+  float Area_em_km2_1 = 318.8;
+  float PIB_1 = 300000;
+  int Numero_de_pontos_turisticos_1 = 50;
+
+  //Segunda Carta
+
+  char Letra_2 = 'A';
+  char Codigo_2 [20] =  "A01";
+  char Nome_da_cidade_2 [20] = "Fortaleza";
+  int populacao_2 = 500000;
+  float Area_em_km2_2 = 318.8;
+  float PIB_2 = 300000;
+  int Numero_de_pontos_turisticos_2 = 50;
+
+printf("Seja Bem Vindo ao jogo Super Trunfo! \n" "Aqui você vai definir as estatísticas de duas cidades de sua escolha! \n" "Boa sorte!\n");
   // Área para entrada de dados
+printf("Digite a letra: \n");
+scanf(" %c", &Letra_1);
+
+printf("Digite o Código: \n");
+scanf(" %s",Codigo_1);
+
+printf("Digite o nome da cidade: \n");
+scanf(" %[^\n]s", Nome_da_cidade_1);
+
+printf("Digite o número da população: \n");
+scanf(" %d",&populacao_1);
+
+printf("Digite a Área em KM²: \n");
+scanf(" %f",&Area_em_km2_1);
+
+printf("Digite o PIB: \n");
+scanf(" %f",&PIB_1);
+
+printf("Digite agora, o número de pontos turísticos: \n");
+scanf(" %d", &Numero_de_pontos_turisticos_1);
+
+
+printf("Ótimo! Agora, vamos para o segundo cartão! \n");
+
+printf("Digite a letra: \n");
+scanf(" %c", &Letra_2);
+
+printf("Digite o Código: \n");
+scanf(" %s",Codigo_2);
+
+printf("Digite o nome da cidade: \n");
+scanf(" %[^\n]s", Nome_da_cidade_2);
+
+printf("Digite o número da população: \n");
+scanf(" %d",&populacao_2);
+
+printf("Digite a Área em KM²: \n");
+scanf(" %f",&Area_em_km2_2);
+
+printf("Digite o PIB: \n");
+scanf(" %f",&PIB_2);
+
+printf("Digite agora, o número de pontos turísticos: \n");
+scanf(" %d", &Numero_de_pontos_turisticos_2);
+
+
+
+
 
   // Área para exibição dos dados da cidade
 
